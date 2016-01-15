cc1a4704e5c54c075131860d600d84b9d273aaa1
Jordan Safer <jsafer@andrew.cmu.edu>
2016-01-14 21:50:10 -0500

    Newest update

--- serialtest.py
+++ serialtest.py
@@ -2,9 +2,6 @@ import serial
 import imuValueExtractors
 import math
 
-ser = serial.Serial('/dev/tty.usbserial-DA01P1HX')
-IM = imuValueExtractors
-
 class niceQ:
     def __init__(self, size):
         self.index = 0
@@ -18,22 +15,37 @@ class niceQ:
     def avgQ(self):
         return sum(self.vals)/self.size
 
+# function to make the readings
+def makeReadings(): 
+    # open a serial connection
+    ser = serial.Serial('/dev/tty.usbserial-DA01P1HX')
+    IM = imuValueExtractors
 
-accels = niceQ(10)
-reads = 0
+    accels = []
+    for i in xrange(4):
+        accels.append(niceQ(10))
 
-while(1):
-    line = ser.readline()
-    acc = IM.getAcceleration(line)
-    total = (acc[0]**2 + acc[1]**2 + acc[2]**2 + acc[3]**2)**.5
-    delta = abs(accels.avgQ() - total)
-    print total
-    if (reads > 30) and delta > .4:
-        pass#print "Throw!", delta
-    accels.pushQ(total)
-    reads += 1
+    reads = 0
 
+    while(1):
+        reads += 1
         
+        line = ser.readline()
+        if(reads < 6):
+            print 6 - reads
+            continue # throw the first few lines to be safe
+        
+        acc = IM.getAcceleration(line)
+        delta = 0
+        for i in xrange(4):
+            dif = abs(accels[i].avgQ() - acc[i])
+            delta += dif
+            accels[i].pushQ(acc[i])
+        if delta > .6:
+            print "Throw"
+        print delta
 
+
+makeReadings()
 
 
