9ea3b07afac63cf5655041d942e20acd10b637c7
EthanTseng123 <ethan.tseng@yahoo.com>
2016-01-15 00:23:49 -0500

    Added ML code

--- interface.py
+++ interface.py
@@ -1,5 +1,9 @@
 import serialtest
 
+dataset = [] #List which stores EVERYONE'S data
+PersonName = #Function which gets the person's name
+collectingData = False #Need to have function which turns this off and on
+
 serialtest.makeReadings(10)
 
 
